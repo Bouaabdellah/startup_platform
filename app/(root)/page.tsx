369e794861fcc356caac85257f3ddbb45cc3d634
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-06-06 19:22:35 +0100

    downgrade tailwind to version 3 and set project configuration again

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,17 @@
 
 export default function Home() {
   return (
-    <div>
-      hello world
+    <div className="py-8 my-4 bg-amber-950">
+      <section className="flex flex-col items-center py-8">
+        <h1 className="heading">
+          pitch your startup
+          <br />
+          connect with entrepreneurs
+        </h1>
+        <p className="text-white text-[18px]">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions 
+        </p>
+      </section>
     </div>
   );
 }

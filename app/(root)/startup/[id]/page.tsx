7c12abc8f2682d9c640e0d357fe2fd506dbaeee2
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-06-13 16:24:06 +0100

    fix next js version and turn it to canary version to supprt PPR and reinstall all other packages to suit canary version, also make some modifications in startup details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -3,6 +3,9 @@ import { client } from '@/sanity/lib/client';
 import { getStartupDetails } from '@/sanity/lib/queries';
 import Image from 'next/image';
 import Link from 'next/link';
+import markdownit from 'markdown-it';
+
+const md = markdownit();
 
 export const experimental_ppr = true;
 
@@ -12,7 +15,7 @@ export default async function StartupDetails({
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
-  const startup = await client.fetch(getStartupDetails, { params: { id } });
+  const startup = await client.fetch(getStartupDetails, { id });
   const {
     _createdAt,
     views,
@@ -24,6 +27,8 @@ export default async function StartupDetails({
     pitch,
     _id,
   } = startup;
+  const parsedContent = md.render(startup.pitch || '');
+
   return (
     <>
       <section className="pink_container flex flex-col items-center">
@@ -38,32 +43,44 @@ export default async function StartupDetails({
           alt="image"
           height={100}
           width={200}
-          className="rounded-3xl w-full"
+          className="rounded-3xl w-full h-auto"
         />
 
-        <div className="flex flex-between mt-10 sm:w-1/2 mx-auto">
-          <Link
-            href={`/user/${author?._id}`}
-            className="flex gap-2 items-center"
-          >
-            <Image
-              src={author.image!}
-              alt={author.name!}
-              width={56}
-              height={56}
-              className="rounded-full w-10 h-10"
+        <div className="sm:w-2/3 mx-auto">
+          <div className="flex flex-between my-10">
+            <Link
+              href={`/user/${author?._id}`}
+              className="flex gap-2 items-center"
+            >
+              <Image
+                src={author.image!}
+                alt={author.name!}
+                width={56}
+                height={56}
+                className="rounded-full w-10 h-10"
+              />
+              <div>
+                <p className="text-20-medium">{author.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{author.username}
+                </p>
+              </div>
+            </Link>
+            <Link href={`/?query=${category?.toLowerCase()}`}>
+              <p className="text-16-medium startup-card_date">{category}</p>
+            </Link>
+          </div>
+          <h3 className="text-30-bold">Pitch Details</h3>
+          {parsedContent ? (
+            <article
+              className="prose max-w-4xl font-work-sans break-all"
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
             />
-            <div>
-              <p className="text-20-medium">{author.name}</p>
-              <p className="text-16-medium !text-black-300">
-                @{author.username}
-              </p>
-            </div>
-          </Link>
-          <Link href={`/?query=${category?.toLowerCase()}`}>
-            <p className="text-16-medium startup-card_date">{category}</p>
-          </Link>
+          ) : (
+            <p className="no-result">No Details Provided</p>
+          )}
         </div>
+        <hr className="divider" />
       </section>
     </>
   );
